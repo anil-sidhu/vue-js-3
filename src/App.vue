d22c69d250590b259a992b90178de5066c2e530f
anil sidhu <webanilsidhu@gmail.com>
2021-02-22 23:14:39 +0530

    check code flow

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
 <img alt="Vue logo" src="./assets/logo.png">
 <HelloWorld msg="Welcome to Your Vue.js App" />
-<Home  />
-<Home  />
-<Home  />
 
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import Home from './components/Home.vue'
 
 export default {
     name: 'App',
     components: {
-        HelloWorld,
-        Home
+        HelloWorld
     }
 }
 </script>
