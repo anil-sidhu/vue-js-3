6efd030f30bba7c3d9cd6a47fdd4b46fc5a5a5cf
anil sidhu <webanilsidhu@gmail.com>
2021-02-22 23:12:32 +0530

    make first component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<img alt="Vue logo" src="./assets/logo.png">
+<HelloWorld msg="Welcome to Your Vue.js App" />
+<Home  />
+<Home  />
+<Home  />
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HelloWorld,
+        Home
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>Home</h1>
+</template>
+<script>
+export default {
+    name:'Home'   
+}
+</script>
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+  color:orange;
+}
+</style>
