8850dbf9b17ac75d128791a8e534a4ab40caea4e
anil sidhu <webanilsidhu@gmail.com>
2021-02-24 20:55:30 +0530

    how to add method

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,25 @@
 <template>
 <h1>Email : {{email}}</h1>
-<h1>Mobile : {{mobile}}</h1>
-
-<h1>Name : {{getName("Anil Sidhu")}}</h1>
-
+<h1>Name : {{getName("Sidhu")}}</h1>
+<h1>Data : {{getData("Bruce").email}}</h1>
 </template>
 
 <script>
 export default {
     name: "Home",
-    data()
-    {
+    data() {
         return {
-            email:"anil@test.com",
-            mobile:9999,
-            getName(x)
-            {
-                return x;
+            email: "anil@test.com",
+        }
+    },
+    methods: {
+        getName(name) {
+            return name;
+        },
+        getData() {
+            return {
+                name: "anil",
+                email: this.email
             }
         }
     }
