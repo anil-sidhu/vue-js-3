2e5d1bf1602cc6a378bc5ea9d97d40cd7538ff34
anil sidhu <webanilsidhu@gmail.com>
2021-02-24 21:27:11 +0530

    reset code

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,28 +1,11 @@
 <template>
-<h1>Email : {{email}}</h1>
-<h1>Name : {{getName("Sidhu")}}</h1>
-<h1>Data : {{getData("Bruce").email}}</h1>
+<h1>Home Component </h1>
 </template>
 
 <script>
 export default {
     name: "Home",
-    data() {
-        return {
-            email: "anil@test.com",
-        }
-    },
-    methods: {
-        getName(name) {
-            return name;
-        },
-        getData() {
-            return {
-                name: "anil",
-                email: this.email
-            }
-        }
-    }
+  
 }
 </script>
 
