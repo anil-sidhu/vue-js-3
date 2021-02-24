4a2686434d9b9458bbb93e8193460b55a23b5730
anil sidhu <webanilsidhu@gmail.com>
2021-02-24 22:32:37 +0530

    event done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,27 @@
 <template>
-<h1>Home Component </h1>
+<h1 v-on:mousemove="getConsole()">Home Component </h1>
+<button v-on:click="getData()">Click Me</button>
+<button v-on:click="getConsole('button 2 click')">Click Me</button>
+<h2>{{count}}</h2>
 </template>
 
 <script>
 export default {
     name: "Home",
-  
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        getData() {
+            this.count = this.count + 1
+        },
+        getConsole() {
+            this.count = this.count - 1
+        }
+    }
+
 }
 </script>
 
