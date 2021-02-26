3203403d5bb72607db7fba120c562da432b4dbcb
anil sidhu <webanilsidhu@gmail.com>
2021-02-27 02:39:48 +0530

    checkbox and radio butto

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <img alt="Vue logo" src="./assets/logo.png">
-<HelloWorld msg="Event in vue js 3" />
+<HelloWorld msg="" />
 <Home />
 
 </template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,24 @@
 <template>
-<h1 v-on:mousemove="getConsole()">Home Component </h1>
-<button v-on:click="getData()">Click Me</button>
-<button v-on:click="getConsole('button 2 click')">Click Me</button>
-<h2>{{count}}</h2>
+<h1>Get Checkbox & Radio Button Value</h1>
+<h3>Technology</h3>
+<label for="java">Java</label>
+<input type="checkbox" value="java" v-model="technology" id="java" />
+
+<label for="node">Node</label>
+<input type="checkbox" value="node" v-model="technology" id="node" />
+
+<label for="php">Php</label>
+<input type="checkbox" value="php" v-model="technology" id="php" />
+
+<br /><br />
+<h3>Profession</h3>
+<label for="student">Student</label>
+<input type="radio" value="student" id="student" v-model="profession"  name="profession" />
+<label for="developer">Developer</label>
+<input type="radio" value="developer" id="developer" v-model="profession" name="profession" />
+<h4>Technology : {{technology}}</h4>
+<h4>I am a {{profession}}</h4>
+
 </template>
 
 <script>
@@ -10,18 +26,10 @@ export default {
     name: "Home",
     data() {
         return {
-            count: 0
-        }
-    },
-    methods: {
-        getData() {
-            this.count = this.count + 1
-        },
-        getConsole() {
-            this.count = this.count - 1
+            technology: [],
+            profession:null
         }
     }
-
 }
 </script>
 
