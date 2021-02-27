fe906cf0726767b65e885c60b24dd246b923af47
anil sidhu <webanilsidhu@gmail.com>
2021-02-27 17:24:26 +0530

    condition added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,32 +1,19 @@
 <template>
-<h1>Get Checkbox & Radio Button Value</h1>
-<h3>Technology</h3>
-<label for="java">Java</label>
-<input type="checkbox" value="java" v-model="technology" id="java" />
+<h1 v-if="show"> Condtions in Vue js</h1>
+<h1 v-else> else part in Vue js</h1>
 
-<label for="node">Node</label>
-<input type="checkbox" value="node" v-model="technology" id="node" />
+<button v-on:click="show=!show" >Click ME</button>
 
-<label for="php">Php</label>
-<input type="checkbox" value="php" v-model="technology" id="php" />
 
-<br /><br />
-<h3>Profession</h3>
-<label for="student">Student</label>
-<input type="radio" value="student" id="student" v-model="profession" name="profession" />
-<label for="developer">Developer</label>
-<input type="radio" value="developer" id="developer" v-model="profession" name="profession" />
-<h4>Technology : {{technology}}</h4>
-<h4>I am a {{profession}}</h4>
 </template>
 
 <script>
 export default {
     name: "Home",
-    data() {
+    data()
+    {
         return {
-            technology: [],
-            profession: null
+            show:true
         }
     }
 }
