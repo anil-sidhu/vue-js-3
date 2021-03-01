d4e74d6750e5b9885a7056cbeb349b37194df31c
anil sidhu <webanilsidhu@gmail.com>
2021-03-01 22:47:59 +0530

    resolved data

---
+++ src/components/Child.vue
@@ -0,0 +1,17 @@
+<template>
+<h2>{{name}}</h2>
+<h2>{{data.email}}</h2>
+<button v-on:click="getData()">Click Me</button>
+
+</template>
+
+<script>
+export default {
+    name: 'Child',
+    props: {
+        name:String,
+        data:Object,
+        getData:Function,
+    }
+}
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,43 +1,27 @@
 <template>
-<h1>For Loop in Vue js</h1>
-<ul>
-    <li v-for="item in technology" :key="item">
-        {{item}}
-    </li>
-</ul>
-<br /><br />
-<ul>
-    <li v-for="item in users" :key="item.name">
-        User name is : {{item.name}} and Email is : {{item.email}}
-    </li>
-</ul>
+<h1>Pass Data to child Component</h1>
+<Child :name="name" :data="user" :getData="getData" />
 </template>
 
 <script>
+import Child from './Child.vue'
 export default {
     name: "Home",
+    components: {
+        Child
+    },
     data() {
         return {
-            technology: [
-                "java",
-                "php",
-                "node",
-                "html",
-                "asp"
-            ],
-            users: [{
-                    name: 'anil',
-                    email: 'anil@test.com'
-                },
-                {
-                    name: 'sam',
-                    email: 'sam@test.com'
-                },
-                {
-                    name: 'peter',
-                    email: 'peter@test.com'
-                }
-            ]
+            name: 'bruce',
+            user: {
+                name: 'bruce',
+                email: 'bruce@test.com'
+            }
+        }
+    },
+    methods: {
+        getData() {
+            alert("Function called")
         }
     }
 }
