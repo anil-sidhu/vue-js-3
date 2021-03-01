cb87bbee9417915729cec6b8f1fef8782d0470ff
anil sidhu <webanilsidhu@gmail.com>
2021-03-01 19:03:47 +0530

    for loop

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,43 @@
 <template>
-<h1 v-if="show"> Condtions in Vue js</h1>
-<h1 v-else> else part in Vue js</h1>
-
-<button v-on:click="show=!show" >Click ME</button>
-
-
+<h1>For Loop in Vue js</h1>
+<ul>
+    <li v-for="item in technology" :key="item">
+        {{item}}
+    </li>
+</ul>
+<br /><br />
+<ul>
+    <li v-for="item in users" :key="item.name">
+        User name is : {{item.name}} and Email is : {{item.email}}
+    </li>
+</ul>
 </template>
 
 <script>
 export default {
     name: "Home",
-    data()
-    {
+    data() {
         return {
-            show:true
+            technology: [
+                "java",
+                "php",
+                "node",
+                "html",
+                "asp"
+            ],
+            users: [{
+                    name: 'anil',
+                    email: 'anil@test.com'
+                },
+                {
+                    name: 'sam',
+                    email: 'sam@test.com'
+                },
+                {
+                    name: 'peter',
+                    email: 'peter@test.com'
+                }
+            ]
         }
     }
 }
