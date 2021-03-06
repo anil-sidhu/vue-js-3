541e2042e39a3f2fd3c9e96ceacfdf118c92db8d
anil sidhu <webanilsidhu@gmail.com>
2021-03-07 02:20:32 +0530

    done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,6 +9,25 @@
     <label>Password : </label>
     <input type="password" placeholder="enter password" v-model="form.password" />
     <br /> <br />
+    <select v-model="form.country">
+        <option>India</option>
+        <option>USA</option>
+    </select>
+    <br /> <br />.
+    <h3>Gender</h3>
+    <label>Male : </label>
+    <input type="radio" name="gender" value="male" v-model="form.gender" />
+    <br /> <br />
+    <label>Female : </label>
+    <input type="radio" name="gender" value="female" v-model="form.gender" />
+    <br /> <br />
+    <h3>Technology</h3>
+    <label>Java : </label>
+    <input type="checkbox" value="java" v-model="form.technology" />
+    <br /> <br />
+    <label>.Net : </label>
+    <input type="checkbox" value=".net" v-model="form.technology" />
+    <br /> <br />
     <button v-on:click="login" type="button">Login</button>
 
 </form>
@@ -21,13 +40,16 @@ export default {
         return {
             form: {
                 email: '',
-                password: ''
+                password: '',
+                country: '',
+                gender: '',
+                technology: []
             }
         }
     },
-    methods:{
-        login()
-        {
+    methods: {
+        login() {
+            
             console.warn(this.form)
         }
     }
