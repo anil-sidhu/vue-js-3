945ca5e58c8184371d2fadd99182ad3441f5fe68
anil sidhu <webanilsidhu@gmail.com>
2021-03-22 20:08:20 +0530

    resolved issue

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="footer">
+<h3>Footer of App</h3>
+<p>Copy rights @ Code Step By Step</p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+.footer{
+    background-color: antiquewhite;
+    color: orange;
+    position: absolute;
+    width: 100%;
+    left: 0;
+    bottom: 0;
+    text-align: center;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,48 +1,16 @@
 <template>
-<Child>
-    <template v-slot:header>
-        <h1>Peter</h1>
-    </template>
-
-    <template v-slot:main>
-      
-    </template>
-    <template v-slot:footer>
-        <button>Buy Now</button>
-    </template>
-</Child>
-<Child>
-    <template v-slot:header>
-        <h1>Anil</h1>
-    </template>
-
-    <template v-slot:main>
-        <p>Code Step By Step</p>
-    </template>
-    <template v-slot:footer>
-        <button>Buy Now</button>
-    </template>
-</Child>
-<Child>
-    <template v-slot:header>
-        <h1>Bhasker</h1>
-    </template>
-
-    <template v-slot:main>
-        <p>Code Step By Step</p>
-    </template>
-    <template v-slot:footer>
-        
-    </template>
-</Child>
+<h1>Teleport components</h1>
+<teleport to="#footer">
+<Footer />
+</teleport>
 </template>
 
 <script>
-import Child from './Child.vue'
+import Footer from './Footer.vue';
 export default {
     name: "Home",
-    components: {
-        Child
+    components:{
+        Footer
     }
 }
 </script>
