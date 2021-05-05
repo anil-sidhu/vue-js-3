4fb517b8cc7a42397f3e41bbff56456f3bdc395c
anil sidhu <webanilsidhu@gmail.com>
2021-05-05 16:53:46 +0530

    api for list done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
-<router-link to="/">Home Page</router-link> <br />
-<router-link to="/login">Login Page</router-link> <br />
-<router-link to="/profile/Peter">Peter's Profile Page</router-link> <br />
-
-<router-view />
+  <h1>User's Data</h1>
+  <ul class="item" v-for="item in list" :key="item.id">
+  <li>{{item.id}}</li>
+  <li>{{item.email}}</li>
+  <li>{{item.first_name}}</li>
+  <li> <img :src="item.avatar" /> </li>
+  </ul>
 </template>
 
 <script>
+import axios from "axios"
 export default {
-    name: 'App',
-}
+  name: "App",
+  data()
+  {
+      return {
+          list:[]
+      }
+  },
+  async  mounted()
+  {
+      let result = await axios.get("https://reqres.in/api/users?page=1");
+      console.warn(result.data.data)
+      this.list=result.data.data
+  }
+
+};
 </script>
+
+<style>
+.item{
+display:flex
+}
+.item img{
+  width:50px;
+}
+.item li{
+  display: inline-block;
+    border: 1px solid;
+    width: 180px;
+    text-align: center;
+    padding: 5px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp} from 'vue'
 import App from './App.vue'
-import router from './routes'
-createApp(App).use(router).mount('#app') 
+
+createApp(App).mount('#app') 
